02224293d6df92e2aded3ff133a35562483394d4
Adrian Souza <repositorio@hands.com.br>
2019-05-29 13:47:05 -0300

    commit menu idiomas

--- assets/index.js
+++ assets/index.js
@@ -19,4 +19,34 @@ $(window).scroll(function() {
 })
 
 
+
+window.onload = function(){
+var menu = document.getElementById("menu");
+menu.addEventListener("click", () => {
+
+    alternar(menu)
+
+});
+
+
+console.log(menu.classList.contains("open"))
+function alternar(menu) {
+    if (menu.classList.contains("open")) {
+        document.getElementById("segundo").style.display = "none";
+        document.getElementById("terceiro").style.display = "none";
+        menu.classList.remove("open")
+        console.log(menu.classList.contains("open"))
+
+    } else {
+        document.getElementById("segundo").style.display = "block";
+        document.getElementById("terceiro").style.display = "block";
+        menu.classList.add("open")
+        console.log("add")
+    }
+
+
+}
+}
+
+
     
